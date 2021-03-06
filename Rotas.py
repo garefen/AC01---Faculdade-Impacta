4685d14a744decda35fa0013850c394f0f37d162
Striderserge <sergio.barros@aluno.faculdadeimpacta.com.br>
2021-03-06 16:00:42 -0300

    add cadastro produto

--- Rotas.py
+++ Rotas.py
@@ -22,7 +22,6 @@ def novo_usuario():
         if len(email) < 1 or len(senha) < 1 or len(nome) < 1:
             raise Exception
         status = cmd_bd.Create_User(email,senha,nome)
-        
 
         # Monta a resposta.
         mensagem = f"O Usuario {nome} com o email{email} foi criada com id {status['id_usuario']}."
@@ -33,22 +32,25 @@ def novo_usuario():
 
 @app.route("/produto/novo", methods = ["GET"])
 def carregar_produto():
-    usuario = {'id_usuario': 'novo', 'nome_usuario': '', 'email_usuario': '', 'senha_usuario': ''}
-    return render_template("form_usuario.html", usuario = usuario)
+    produto = {'id_produto': 'novo', 'nome_produto': '', 'tipo_produto': '', 'foto_produto': '','preco_compra_produto': '', 'preco_venda_produto': '','quantidade_produto': '' }
+    return render_template("form_usuario.html", produto = produto)
 
 @app.route('/produto/novo', methods=['POST'])
 def novo_produto():
     try:
-        email = request.form["email"]
-        senha = request.form["senha"]
-        nome = request.form["nome"]
-        if len(email) < 1 or len(senha) < 1 or len(nome) < 1:
+        nome = request.forms["nome"]
+        tipo = request.form["tipo"]
+        foto = request.form["foto"]
+        preco_compra = request.form["preco_compra"]
+        preco_venda = request.form["preco_venda"]
+        quantidade = request.form["quantidade"]
+        if len(nome) < 1 or len(tipo) < 1 or len(preco_compra) < 1 or len(preco_venda) < 1 or len(quantidade) < 1:
             raise Exception
-        status = cmd_bd.Create_User(email,senha,nome)
+        status = cmd_bd.Create_Produto(nome,tipo,foto,preco_compra,preco_venda,quantidade)
         
 
         # Monta a resposta.
-        mensagem = f"O Usuario {nome} com o email{email} foi criada com id {status['id_usuario']}."
+        mensagem = f"O produto {nome}  foi criado com id {status['id_produto']}."
         return render_template("menu.html", mensagem = mensagem)
     except Exception:
         mensagem = "Algo de errado não está certo."
